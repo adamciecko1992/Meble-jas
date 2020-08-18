536e6193806f44321ca1f57bee8ec8e0b8ed5aec
adamciecko1992 <adam.ciecko@hotmail.com>
2020-08-18 23:30:27 +0200

    observer

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.scss"],
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe([Breakpoints.HandsetLandscape, Breakpoints.HandsetPortrait])
+      .subscribe((result) => {
+        console.log(result);
+      });
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { LayoutModule } from "@angular/cdk/layout";
 
 import { MatButtonModule } from "@angular/material/button";
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomePageComponent } from "./home-page/home-page.component";
-import { HeroComponent } from './hero/hero.component';
-import { AboutComponent } from './about/about.component';
+import { HeroComponent } from "./hero/hero.component";
+import { AboutComponent } from "./about/about.component";
 
 @NgModule({
   declarations: [HomePageComponent, HeroComponent, AboutComponent],
-  imports: [CommonModule, HomeRoutingModule, MatButtonModule],
+  imports: [CommonModule, HomeRoutingModule, MatButtonModule, LayoutModule],
 })
 export class HomeModule {}
